c196ef3085b8f713ca7f71d7939c11d3295ba5df
JPCalsavara <jpcalsavara@gmail.com>
2025-04-25 15:13:37 -0300

    Apagando arquivos repetidos ou modificando de js para ts

--- frontend/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className=" bottom-0 left-0 w-{100%} bg-gray-800 text-gray-400 text-center py-4">
-        <p>© {new Date().getFullYear()} João Pedro Calsavara. Todos os direitos reservados.</p>
-      </footer>
-    );
-  }
